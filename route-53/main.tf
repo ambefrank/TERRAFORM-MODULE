6ebc3fcfbf1866ecfd0664e05724741ab1e98691
ambefrankline <www.ambefrank@gmail.com>
2023-05-27 02:02:00 -0400

    created route-53 mdolue

--- route-53/main.tf
+++ route-53/main.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "hosted_zone" {
 
 # create a record set in route 53
 resource "aws_route53_record" "site_domain" {
-  zone_id = data.aws_route53_zone.hosted_zone.zone.id
+  zone_id = var.aws_route53_zone.hosted_zone.zone.id
   name    = var.record_name
   type    = "A"
 
--- route-53/variables.tf
+++ route-53/variables.tf
@@ -3,3 +3,4 @@ variable "domain_name" {}
 variable "record_name" {}
 variable "application_load_balancer_dns_name" {}
 variable "application_load_balancer_zone_id" {}
+variable "aws_route53_zone.hosted_zone.zone.id" {}
